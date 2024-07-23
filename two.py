012399031bca9a720d60fe22a234fb55aa8561bb
olegsl2011 <oleg.sokolenko@xloo.com>
2024-07-23 16:01:17 +0300

    done with home work

---
+++ two.py
@@ -0,0 +1,30 @@
+def binary_search_with_upper_bound(arr, target):
+    low, high = 0, len(arr) - 1
+    iterations = 0
+    upper_bound = arr[-1]  # Початкове значення - максимальний елемент масиву
+
+    while low <= high:
+        iterations += 1
+        mid = (low + high) // 2
+
+        if arr[mid] < target:
+            low = mid + 1
+        else:
+            upper_bound = arr[mid]
+            high = mid - 1
+
+    return iterations, upper_bound
+
+
+if __name__ == "__main__":
+    # Тестування функції
+    arr = [1.1, 1.3, 2.5, 3.8, 4.6, 5.9]
+    print(
+        f"Виведе: (кількість ітерацій, верхню межу) {binary_search_with_upper_bound(arr, 15.1)}"
+    )  
+    print(
+        f"Виведе: (кількість ітерацій, верхню межу) {binary_search_with_upper_bound(arr, 4)}"
+    )
+    print(
+        f"Виведе: (кількість ітерацій, верхню межу) {binary_search_with_upper_bound(arr, 6.0)}"
+    )  
